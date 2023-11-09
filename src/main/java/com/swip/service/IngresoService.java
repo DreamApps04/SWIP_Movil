da4775b0a472d07ceb75814bea3380e44acf982a
JavierMadrigal22 <84678572+JavierMadrigal22@users.noreply.github.com>
2023-11-09 17:43:29 -0600

    Aca se estara implementando el MVC de ingresos y actualizacion en la DB

---
+++ src/main/java/com/swip/service/IngresoService.java
@@ -0,0 +1,16 @@
+
+package com.swip.service;
+ 
+import com.swip.domain.Ingreso;
+import java.util.List;
+
+public interface IngresoService {
+
+    public List<Ingreso> getAllIngresos();
+    
+    public Ingreso getIngreso(Ingreso ingreso);
+    
+    public void save(Ingreso ingreso);
+    
+    public void delete(Ingreso ingreso);
+}
