05f57fc2abef9d7c966b5c3309644b5748037358
JosethAG <93845320+JosethAG@users.noreply.github.com>
2023-11-09 14:39:33 -0600

    Fix

--- src/main/java/com/swip/controller/CreditoController.java
+++ src/main/java/com/swip/controller/CreditoController.java
@@ -1,7 +1,7 @@
 package com.swip.controller;
 
-import com.swip.domain.Usuario;
-import com.swip.service.UsuarioService;
+import com.swip.domain.Credito;
+import com.swip.service.CreditoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +12,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+@Controller
+@Slf4j
+@RequestMapping("/credito")
 public class CreditoController {
     
+    @Autowired
+    private CreditoService creditoService;
+
+    @GetMapping("/creditos")
+    public String inicio(Model model) {
+        var credito = creditoService.getCreditos(0);
+        model.addAttribute("credito", credito);
+        model.addAttribute("totalCreditos", credito.size());
+        return "/credito/creditos";
+    }
+    
+    @GetMapping("/nuevo")
+    public String creditoNuevo(Credito credito) {
+        return "/credito/modifica";
+    }
+    
+    @PostMapping("/guardar")
+    public String creditoGuardar(Credito credito,
+            @RequestParam("imagenFile") MultipartFile imagenFile) {        
+        if (!imagenFile.isEmpty()) {
+            creditoService.save(credito);
+        }
+        creditoService.save(credito);
+        return "redirect:/credito/creditos";
+    }
+
+    @GetMapping("/eliminar/{idCredito}")
+    public String creditoEliminar(Credito credito) {
+        creditoService.delete(credito);
+        return "redirect:/credito/creditos";
+    }
+
+    @GetMapping("/modificar/{idCredito}")
+    public String creditoModificar(Credito credito, Model model) {
+        credito = creditoService.getCredito(credito);
+        model.addAttribute("credito", credito);
+        return "/credito/modifica";
+    }
 }
--- src/main/java/com/swip/service/CreditoService.java
+++ src/main/java/com/swip/service/CreditoService.java
@@ -1,6 +1,20 @@
 package com.swip.service;
 
+import com.swip.domain.Credito;
+import java.util.List;
+
 
 public interface CreditoService {
+    // Se obtiene un listado de productos en un List
+    public List<Credito> getCreditos(long idcredito);
     
+   // Se obtiene un Credito, a partir del id de un producto
+    public Credito getCredito(Credito credito);
+    
+    // Se inserta un nuevo producto si el id del producto esta vacío
+    // Se actualiza un producto si el id del producto NO esta vacío
+    public void save(Credito credito);
+    
+    // Se elimina el producto que tiene el id pasado por parámetro
+    public void delete(Credito credito);
 }
